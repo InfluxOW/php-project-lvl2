6cf1dd1a4b804aad436dda712864a2b7dfae097b
InfluxOW <krochak_n@mail.ru>
2020-01-24 02:25:17 +1000

    refactoring

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/28a51604ba3626ea12d3/maintainability)](https://codeclimate.com/github/InfluxOW/php-project-lvl2/maintainability)
 [![Build Status](https://travis-ci.org/InfluxOW/php-project-lvl2.svg?branch=master)](https://travis-ci.org/InfluxOW/php-project-lvl2)
+
+$ installation process\
+[![asciicast](https://asciinema.org/a/DeHm2Rjew4AxdrCSYYtAhMGH7.svg)](https://asciinema.org/a/DeHm2Rjew4AxdrCSYYtAhMGH7)\
+$ comparing 2 flat `.json` files with **plain**, **text** and **json** renderers\
+[![asciicast](https://asciinema.org/a/Jq8OnAiFRZxDLV0j2A6Eea5Kg.svg)](https://asciinema.org/a/Jq8OnAiFRZxDLV0j2A6Eea5Kg)\
+$ comparing 2 nested `.yaml` files with **plain**, **text** and **json** renderers\
+[![asciicast](https://asciinema.org/a/MVBw8RukpIPhxYGIXOR6Lk1d0.svg)](https://asciinema.org/a/MVBw8RukpIPhxYGIXOR6Lk1d0)
+
+---
+
+## How To Install
+`composer global require influx/php-project-lvl2`
+
+---
+
+## Usage
+    gendiff (-h|--help)
+    gendiff (-v|--version)
+    gendiff [--format <fmt>] <firstFile> <secondFile>
--- src/Ast.php
+++ src/Ast.php
@@ -4,36 +4,34 @@ namespace Differ\Ast;
 
 function genDiffAst($beforeData, $afterData)
 {
-    // getting unique keys from both data arrays
     $keys = array_unique(array_merge(array_keys($beforeData), array_keys($afterData)));
 
     $result = array_reduce($keys, function ($acc, $key) use ($beforeData, $afterData) {
         $valueBefore = $beforeData[$key] ?? null;
         $valueAfter = $afterData[$key] ?? null;
 
-        // value removed
         if (!array_key_exists($key, $afterData)) {
             $acc[] = ['type' => 'removed', 'key' => $key, 'valueBefore' => $valueBefore];
             return $acc;
         }
-        // value added
+
         if (!array_key_exists($key, $beforeData)) {
             $acc[] = ['type' => 'added', 'key' => $key, 'valueAfter' => $valueAfter];
             return $acc;
         }
-        // value has children
+
         if (is_array($valueBefore) && is_array($valueAfter)) {
             $children = genDiffAst($valueBefore, $valueAfter);
             $acc[] = ['type' => 'nested', 'key' => $key, 'valueBefore' => $valueBefore,
             'valueAfter' => $valueAfter, 'children' => $children];
             return $acc;
         }
-        // value unchanged
+
         if ($valueBefore === $valueAfter) {
             $acc[] = ['type' => 'unchanged', 'key' => $key, 'valueBefore' => $valueBefore, 'valueAfter' => $valueAfter];
             return $acc;
         }
-        // value changed
+
         if (array_key_exists($key, $beforeData) && array_key_exists($key, $afterData) && $valueBefore !== $valueAfter) {
             $acc[] = ['type' => 'changed', 'key' => $key, 'valueBefore' => $valueBefore, 'valueAfter' => $valueAfter];
             return $acc;
